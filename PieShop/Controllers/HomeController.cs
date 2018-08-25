e217548472803a6bc592b2ae2296ba2414cd33bd
Jhavi <javiersegovia15@hotmail.com>
2018-08-25 04:57:26 -0300

    updating home controller

--- PieShop/Controllers/HomeController.cs
+++ PieShop/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using PieShop.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,6 +11,14 @@ namespace PieShop.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IPieRepository _pieRepository;
+
+        public HomeController( IPieRepository pieRepository)
+        {
+            _pieRepository = pieRepository;
+        }
+
+
         // GET: /<controller>/
         public IActionResult Index()
         {
